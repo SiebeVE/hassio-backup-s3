2a11f8c24e8bdf6de7d8fafeca68c40e8c85f36d
Siebe Vanden Eynden <SiebeVE@users.noreply.github.com>
2023-11-25 19:14:10 +0100

    Fixed

--- backup-s3/run.sh
+++ backup-s3/run.sh
@@ -6,9 +6,6 @@ BUCKET=`jq -r .bucketname /data/options.json`
 ENDPOINT=`jq -r .endpoint /data/options.json`
 REGION=`jq -r .region /data/options.json`
 
-AWS_IGNORE_CONFIGURED_ENDPOINT_URLS=true
-AWS_ENDPOINT_URL=$ENDPOINT
-
 MOST_RECENT_FILE=$(ls -lt /backup | awk 'NR==2{print $9}')
 
 mkdir -p ~/.aws
@@ -16,6 +13,8 @@ mkdir -p ~/.aws
 aws configure set aws_access_key_id $KEY
 aws configure set aws_secret_access_key $SECRET
 
-aws s3 cp /backup/$MOST_RECENT_FILE s3://$BUCKET --storage-class GLACIER
+echo "Moving backup $MOST_RECENT_FILE" 
+
+aws s3 cp /backup/$MOST_RECENT_FILE s3://$BUCKET --storage-class GLACIER --endpoint-url $ENDPOINT --region $REGION
 
 echo "Done"
